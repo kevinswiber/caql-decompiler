1815735232e1ca9c88c9cab8be3ae75f741e7f2b
Kevin Swiber <kswiber@gmail.com>
2015-02-05 15:59:50 -0500

    Supporting IS and IS NOT MISSING

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ var CaqlDecompiler = require('./decompiler');
 var decompiler = new CaqlDecompiler();
 
 var query =   'select name, founded_year, total_money_raised as worth '
-            + 'where founded_year >= 1999 and name not like "%air%" '
+            + 'where type is missing and founded_year >= 1999 and name not like "%air%" '
             + 'order by founded_year desc, name';
 
 var ast = Parser.parse(query);
